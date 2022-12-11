8b3fe7beec8ce99fa4b1509933adade67700463c
JohnyJonson <118004367+JohnyJonson@users.noreply.github.com>
2022-12-11 11:40:27 +0100

    Update and rename source to source.lua

--- source.lua
+++ source.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "bab", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local KeyWindow = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 --[[
 Name = <string> - The name of the UI.
@@ -12,25 +12,50 @@ IntroIcon = <string> - URL to the image you want to use in the intro animation.
 Icon = <string> - URL to the image you want displayed on the window.
 CloseCallback = <function> - Function to execute when the window is closed.
 ]]
-local Tab = Window:MakeTab({
-	Name = "MainTab",
+local Tab = KeyWindow:MakeTab({
+	Name = "Key",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+_G.Key = "token-key?php.index=private"
+_G.KeyInput = "string"
+Tab:AddTextbox({
+	Name = "Key",
+	Default = "default box input",
+	TextDisappear = true,
+	Callback = function(Value)
+		_G.KeyInput = Value 
+			if _G.KeyInput == _G.Key then
+				OrionLib:MakeNotification({
+	Name = "Key",
+	Content = "Correct Key!",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+task.wait(2)
+OrionLib:Destroy()
+--[[
+Title = <string> - The title of the notification.
+Content = <string> - The content of the notification.
+Image = <string> - The icon of the notification.
+Time = <number> - The duration of the notfication.
+]]
+				end
+	end	  
+})
 
+--[[
+Name = <string> - The name of the textbox.
+Default = <string> - The default value of the textbox.
+TextDisappear = <bool> - Makes the text disappear in the textbox after losing focus.
+Callback = <function> - The function of the textbox.
+]]
 --[[
 Name = <string> - The name of the tab.
 Icon = <string> - The icon of the tab.
 PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
 ]]
-Tab:AddToggle({
-	Name = "This is a toggle!",
-	Default = false,
-	Callback = function(Value)
-		print(Value)
-		
-	end    
-})
+
 
 --[[
 Name = <string> - The name of the toggle.
